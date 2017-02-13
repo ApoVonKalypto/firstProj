9cb4094ba915770d58ad8f53f3c7d15bcfeb9814
ApoVonKalypto <apokalypto@live.de>
2017-02-13 14:44:15 +0100

    moah....

--- mixed_app/mixed_app/firstprojFun.h
+++ mixed_app/mixed_app/firstprojFun.h
@@ -1,25 +1,26 @@
 #pragma once
 
 
-int fScan(void) {
+int fScan() {
 
 	int dpi = 0, farbtiefe = 0;
 	float breite = 0, hoehe = 0;
 	double groesse = 0.00;
 	printf("---------- Scan Berechnung ----------\n");
-	printf("Bitte geben die die DPI ein: ");
+	
+	printf("Bitte geben die die DPI ein: \n");
 	scanf("%i", &dpi);
 
-	printf("Bittegeben die die Farbtiefe ein: ");
+	printf("Bittegeben die die Farbtiefe ein: \n");
 	scanf("%i", &farbtiefe);
 
-	printf("Bittegeben die die Breite in cm ein: ");
-	scanf("%i", &breite);
+	printf("Bittegeben die die Breite in cm ein: \n");
+	scanf("%f", &breite);
 
-	printf("Bittegeben die die Hoehe in cm ein: ");
-	scanf("%i", &hoehe);
+	printf("Bittegeben die die Hoehe in cm ein: \n");
+	scanf("%f", &hoehe);
 	printf("---------- Ausgabe ----------\n");
-	groesse = ((((breite * dpi) / 2.54) * (hoehe * dpi) / 2.54) * farbtiefe) / 8 / 1024 / 1024;
+	groesse = (((breite * dpi) / 2.54 * (hoehe * dpi) / 2.54) * farbtiefe) / 8 / 1024 / 1024;
 	
 	printf("Die zu erwartende Dateigr\x94\xE1\e entspricht\n %.4lf MIB\n",groesse);
 	
@@ -48,6 +49,6 @@ int fKinder(void) {
 	rest = eier%kinder;
 
 	printf("Es wurden %i Kinder eingeladen und jedes hat %i \x9a\x62\erraschungseier erhalten\n"
-		"Es Blieben noch %i \x81\x62rig\n", kinder, erhalten, rest);
+		"Es blieben noch %i \x81\x62rig\n", kinder, erhalten, rest);
 	return 0;
 }
